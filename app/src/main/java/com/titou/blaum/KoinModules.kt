b56a8dc39ddc59a2cdba21e510ed5a9eefd62143
Titouan Rapin <titouan.rapin@gmail.com>
2021-07-25 14:02:14 +0200

    feat: add use case koin module to app modules

--- app/src/main/java/com/titou/blaum/KoinModules.kt
+++ app/src/main/java/com/titou/blaum/KoinModules.kt
@@ -3,12 +3,14 @@ package com.titou.blaum
 import com.titou.blaum.data_source.di.DatabaseModule
 import com.titou.blaum.data_source.di.TitlesApiModule
 import com.titou.blaum.presentation.di.ActivityModule
+import com.titou.blaum.use_cases.di.UseCasesModule
 
 object KoinModules {
 
     fun toList() = listOf(
         ActivityModule.module,
         DatabaseModule.module,
-        TitlesApiModule.module
+        TitlesApiModule.module,
+        UseCasesModule.module
         )
 }
